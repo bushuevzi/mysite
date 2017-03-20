6ab724d83fa3e3fa077ac02e1dfe7eb634f76ac4
Zakhar Bushuev <bushuevzi@mail.ru>
2017-03-20 23:52:32 +0300

    Add template. Refactor view function

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from mysite.views import current_datetime, hours_ahead
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^time/$', current_datetime),
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
 ]
---
+++ mysite/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+import datetime
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    return render_to_response('current_datetime.html', {'current_date': now})
+
+def hours_ahead(request, offset):
+    #offset = int(offset)
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
