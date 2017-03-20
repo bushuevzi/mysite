4565aa0c1cb7db294e0cb62b5db46da626da3d21
bushuevzi <bushuevzi@mail.ru>
2017-03-21 01:10:32 +0300

    use template inheritance

--- mysite/views.py
+++ mysite/views.py
@@ -7,7 +7,6 @@ def current_datetime(request):
     return render_to_response('current_datetime.html', locals())
 
 def hours_ahead(request, offset):
-    #offset = int(offset)
+    offset = int(offset)
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-    return HttpResponse(html)
+    return render_to_response('hours_ahead.html', {'hour_offset': offset, 'next_time': dt})
